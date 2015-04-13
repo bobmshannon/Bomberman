b7c5626552e544c1a0f025151d7e2ff32b926865
bobmshannon <rshannon@buffalo.edu>
2015-04-13 10:46:26 -0400

    hide_cursor tweak

--- lab7.s
+++ lab7.s
@@ -33,7 +33,7 @@ refresh_timer_fired DCD 0x00000000
 	ALIGN
 		
 intro_message = "Update this message!\n\rPress <SPACE> to begin game",0
-hide_cursor = "\x1B\x5B\x3F\x32\x35\x6C"
+hide_cursor = "\x1B[?25l", 0
 	ALIGN
 		
 lab7	 	
@@ -44,6 +44,7 @@ lab7
 	BL uart_init
 	BL interrupt_init
 	BL timer_init
+	
 	
 	LDR v1, = hide_cursor
 	BL output_string
